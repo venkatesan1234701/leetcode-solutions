2ea8283c3015ec4fc9396ad18efb28af59ab84a5
venkatesh <amma13066@gmail.com>
2026-01-24 16:28:32 +0530

    Time: 3 ms (41.63%) | Memory: 64.8 MB (23.29%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let sum = nums[0]
+    let maxsum = nums[0]
+for(let i=1;i<nums.length;i++){
+        sum= Math.max(nums[i],sum+nums[i])
+        maxsum = Math.max(maxsum,sum)   
+}
+return maxsum
+};
