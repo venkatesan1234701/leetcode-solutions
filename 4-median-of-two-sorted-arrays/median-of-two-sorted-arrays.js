300ee864109803d25832e06ed2bc31dfdb2525bd
venkatesh <amma13066@gmail.com>
2026-01-31 11:59:13 +0530

    Time: 5 ms (34.15%) | Memory: 60.3 MB (29.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -4,14 +4,20 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-let arr = nums1.concat(nums2)
-arr.sort((a,b) => a-b)
+ let arr = []
+ for(let i=0;i<nums1.length;i++){
+    arr.push(nums1[i])
+ }
+ for(let i=0;i<nums2.length;i++){
+    arr.push(nums2[i])
+ }
+ arr.sort((a,b) => a-b)
 let n = arr.length
-let ans = 0
+let l = 0
 if(n%2===0){
-   ans = (arr[n/2-1] + arr[n/2])/2
+    l = (arr[n/2-1] + arr[n/2])/2
 }else{
-    ans = arr[Math.floor(n/2)]
+    l = arr[Math.floor(n/2)]
 }
-return ans
+return l
 };
