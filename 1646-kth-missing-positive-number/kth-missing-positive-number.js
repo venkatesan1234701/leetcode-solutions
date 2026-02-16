c8fc4385dae25f03a2047d493cfb8ec21578e3fe
venkatesh <amma13066@gmail.com>
2026-02-16 10:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (27.17%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+for(let i=0;i<arr.length;i++){
+    if(arr[i]<=k){
+        k++
+    }else{
+        break;
+    }
+}
+return k
+};
