58c5d4bcb4ab0da89ea2f198fc65def79c767f55
venkatesh <amma13066@gmail.com>
2026-03-02 09:57:11 +0530

    Time: 14 ms (38.84%) | Memory: 62.6 MB (78.90%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -3,20 +3,14 @@
  * @return {string}
  */
 var removeDuplicates = function(s) {
-    let arr = s.split('');
-    let changed = true;
-
-    while (changed) {
-        changed = false;
-
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] === arr[i + 1]) {
-                arr.splice(i, 2);  
-                changed = true;
-                break;
-            }
+    let stack = []
+    for (let i of s) {
+        if (i === stack.at(-1)) {
+            stack.pop(i)
+            continue
+        } else {
+            stack.push(i)
         }
     }
-
-    return arr.join('');
+    return stack.join("")
 };
